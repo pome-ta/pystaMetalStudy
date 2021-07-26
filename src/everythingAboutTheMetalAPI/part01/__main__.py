9e40c4b72c4585e4f52eda1db9d7c40e9b3d63b0
pome-ta <pome-ta@users.noreply.github.com>
2021-07-26 21:35:34 +0900

    rename

--- src/everythingAboutTheMetalAPI/part01/__main__.py
+++ src/everythingAboutTheMetalAPI/part01/__main__.py
@@ -1,5 +1,5 @@
 import ctypes
-from objc_util import c, create_objc_class, ObjCClass, ObjCInstance
+from objc_util import c, ObjCClass, ObjCInstance
 import ui
 
 #import pdbg
@@ -13,7 +13,7 @@ MTLCreateSystemDefaultDevice.argtypes = []
 MTLCreateSystemDefaultDevice.restype = ctypes.c_void_p
 
 
-class MetalView(ui.View):
+class View(ui.View):
   def __init__(self, *args, **kwargs):
     ui.View.__init__(self, *args, **kwargs)
     self.bg_color = 'maroon'
@@ -22,56 +22,15 @@ class MetalView(ui.View):
   def view_did_load(self):
     mtkView = MTKView.alloc()
     _device = MTLCreateSystemDefaultDevice()
-    _frame = ((0.0, 0.0), (100.0, 100.0))
 
     devices = ObjCInstance(_device)
-    mtkView.initWithFrame_device_(_frame, devices)
+    mtkView.initWithFrame_device_(((0, 0), (100, 100)), devices)
     mtkView.setAutoresizingMask_((1 << 1) | (1 << 4))
-    renderer = self.renderer_init(PyRenderer, mtkView)
-    mtkView.delegate = renderer
 
     self.objc_instance.addSubview_(mtkView)
 
-  def renderer_init(self, delegate, _mtkView):
-    renderer = delegate.alloc().init()
-    renderer.device = _mtkView.device()
-
-    return renderer
-
-
-# --- MTKViewDelegate
-def drawInMTKView_(_self, _cmd, _view):
-  self = ObjCInstance(_self)
-  view = ObjCInstance(_view)
-  drawable = view.currentDrawable()
-  rpd = view.currentRenderPassDescriptor()
-  rpd.colorAttachments().objectAtIndexedSubscript(
-    0).texture = view.currentDrawable().texture()
-
-  rpd.colorAttachments().objectAtIndexedSubscript(0).clearColor = (0.0, 0.5,
-                                                                   0.5, 1.0)
-
-  rpd.colorAttachments().objectAtIndexedSubscript(0).loadAction = 2  # .clear
-
-  commandBuffer = self.device.newCommandQueue().commandBuffer()
-  commandEncoder = commandBuffer.renderCommandEncoderWithDescriptor_(rpd)
-  commandEncoder.endEncoding()
-  commandBuffer.presentDrawable_(drawable)
-  commandBuffer.commit()
-
-
-def mtkView_drawableSizeWillChange_(_self, _cmd, _view, _size):
-  self = ObjCInstance(_self)
-  view = ObjCInstance(_view)
-
-
-PyRenderer = create_objc_class(
-  name='PyRenderer',
-  methods=[drawInMTKView_, mtkView_drawableSizeWillChange_],
-  protocols=['MTKViewDelegate'])
 
 if __name__ == '__main__':
-  # xxx: 数秒走らせるとエラーになる
-  view = MetalView()
+  view = View()
   view.present(style='fullscreen', orientations=['portrait'])
 
