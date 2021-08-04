7315b97c4569177f24ac095049763ef6600bcec0
pome-ta <pome-ta@users.noreply.github.com>
2021-08-04 15:28:29 +0900

    refactor

--- src/everythingAboutTheMetalAPI/chapter10/__main__.py
+++ src/everythingAboutTheMetalAPI/chapter10/__main__.py
@@ -1,42 +1,36 @@
 import pathlib
 import ctypes
 
-from objc_util import c, create_objc_class, ObjCClass, ObjCInstance, on_main_thread
+from objc_util import c, create_objc_class, ObjCClass, ObjCInstance
 import ui
 
 import pdbg
 
 shader_path = pathlib.Path('./Shaders.metal')
 
-
 # --- load objc classes
 MTKView = ObjCClass('MTKView')
 MTLCompileOptions = ObjCClass('MTLCompileOptions')
-
 
 # --- initialize MetalDevice
 MTLCreateSystemDefaultDevice = c.MTLCreateSystemDefaultDevice
 MTLCreateSystemDefaultDevice.argtypes = []
 MTLCreateSystemDefaultDevice.restype = ctypes.c_void_p
 
-
 err_ptr = ctypes.c_void_p()
 
 
-
 class MetalView(ui.View):
-  #@on_main_thread
   def __init__(self, *args, **kwargs):
     ui.View.__init__(self, *args, **kwargs)
     self.bg_color = 'maroon'
     self.view_did_load()
 
-  #@on_main_thread
   def view_did_load(self):
     mtkView = MTKView.alloc()
     _device = MTLCreateSystemDefaultDevice()
     devices = ObjCInstance(_device)
-    
+
     # todo: 端末サイズにて要調整
     '''
     _uw, _uh = ui.get_window_size()
@@ -51,65 +45,59 @@ class MetalView(ui.View):
     #mtkView.setAutoresizingMask_((1 << 1) | (1 << 4))
     renderer = self.renderer_init(PyRenderer, mtkView)
     mtkView.delegate = renderer
-    
+
     #mtkView.enableSetNeedsDisplay = True
     mtkView.framebufferOnly = False
     #mtkView.setNeedsDisplay()
-    
+
     self.objc_instance.addSubview_(mtkView)
 
   def renderer_init(self, delegate, _mtkView):
     renderer = delegate.alloc().init()
-    
-    renderer.device = _mtkView.device()
-    renderer.commandQueue = renderer.device.newCommandQueue()
-    
+
+    device = _mtkView.device()
+    renderer.commandQueue = device.newCommandQueue()
 
     # --- registerShaders
     source = shader_path.read_text('utf-8')
-    library = renderer.device.newLibraryWithSource_options_error_(source, err_ptr, err_ptr)
-    
+    library = device.newLibraryWithSource_options_error_(source, err_ptr, err_ptr)
     kernel = library.newFunctionWithName_('compute')
-    
+
     # maxTotalThreadsPerThreadgroup: 1024
     # threadExecutionWidth: 32
-    renderer.cps = renderer.device.newComputePipelineStateWithFunction_error_(kernel, err_ptr)
+    renderer.cps = device.newComputePipelineStateWithFunction_error_(kernel, err_ptr)
 
     return renderer
-    
 
 
 # --- MTKViewDelegate
-#@on_main_thread
 def drawInMTKView_(_self, _cmd, _view):
   self = ObjCInstance(_self)
   view = ObjCInstance(_view)
-  
+
   drawable = view.currentDrawable()
-  
+
   commandBuffer = self.commandQueue.commandBuffer()
   commandEncoder = commandBuffer.computeCommandEncoder()
   commandEncoder.setComputePipelineState_(self.cps)
-  
+
   commandEncoder.setTexture_atIndex_(drawable.texture(), 0)
-  
+
   _width = 8
   _height = 8
   _depth = 1
-  
+
   threadGroupCount = (_width, _height, _depth)
-  
+
   t_w = drawable.texture().width()
   t_h = drawable.texture().height()
-  #print(drawable.texture().width())
   threadGroups = (t_w // _width, t_h // _height, 1)
   commandEncoder.dispatchThreads_threadsPerThreadgroup_(threadGroups, threadGroupCount)
-  
+
   commandEncoder.endEncoding()
   commandBuffer.presentDrawable_(drawable)
   commandBuffer.commit()
-  #pdbg.state(commandEncoder)
-  
+  #commandBuffer.waitUntilCompleted()
 
 
 def mtkView_drawableSizeWillChange_(_self, _cmd, _view, _size):
@@ -125,3 +113,4 @@ PyRenderer = create_objc_class(
 if __name__ == '__main__':
   view = MetalView()
   view.present(style='fullscreen', orientations=['portrait'])
+
