63de23d6627880b041f3296309c5679dbff090bb
pome-ta <pome-ta@users.noreply.github.com>
2021-08-06 02:39:32 +0900

    touch event

--- src/everythingAboutTheMetalAPI/chapter12/__main__.py
+++ src/everythingAboutTheMetalAPI/chapter12/__main__.py
@@ -16,21 +16,43 @@ MTLCreateSystemDefaultDevice = c.MTLCreateSystemDefaultDevice
 MTLCreateSystemDefaultDevice.argtypes = []
 MTLCreateSystemDefaultDevice.restype = ctypes.c_void_p
 
-
 err_ptr = ctypes.c_void_p()
 
 
 class MetalView(ui.View):
   def __init__(self, *args, **kwargs):
     ui.View.__init__(self, *args, **kwargs)
-    self.bg_color = '#242424'
     self.view_did_load()
-    
-  def touch_began(self, touch):
-    print(touch.location)
+    self.touch_txt = ui.Label()
+    self.touch_txt.width = self.s_size
+    self.add_subview(self.touch_txt)
 
+  def touch_began(self, touch):
+    _x, _y = [i / self.s_size for i in touch.location]
+    #self.touch_txt.text = f'{str(touch.location)}, {_x}:{_y}'
+    self.mouse[0] = _x
+    self.mouse[1] = _y
+    
+    
   def touch_moved(self, touch):
-    pass
+    _x, _y = touch.location
+    # xxx: 範囲外数値はザル
+    if 0 < _x < self.s_size and 0 < _y < self.s_size:
+      _x /= self.s_size
+      _y /= self.s_size
+    elif _x < 0:
+      _x = _y = 0.0
+    elif _y < 0:
+      _y = _x = 0.0
+    elif _x > self.s_size:
+      _x = _y = 1.0
+    elif _y > self.s_size:
+      _y = _x = 1.0
+    else:
+      _x = _y = 0.0
+    #self.touch_txt.text = f'{_x}:{_y} :{str(touch.location)}'
+    self.mouse[0] = _x
+    self.mouse[1] = _y
 
   def view_did_load(self):
     mtkView = MTKView.alloc()
@@ -38,7 +60,7 @@ class MetalView(ui.View):
     devices = ObjCInstance(_device)
 
     # todo: 端末サイズにて要調整
-    s_size = min(ui.get_window_size()) * 0.96
+    s_size = int(min(ui.get_window_size()) * 0.88)
     _frame = ((0.0, 0.0), (s_size, s_size))
     self.width = s_size
     self.height = s_size
@@ -69,9 +91,14 @@ class MetalView(ui.View):
     renderer.cps = device.newComputePipelineStateWithFunction_error_(kernel, err_ptr)
     renderer.tew = renderer.cps.threadExecutionWidth()
     renderer.mttpt = renderer.cps.maxTotalThreadsPerThreadgroup()
-    
+
     renderer.timer = ctypes.c_float(0.0)
     renderer.timerBuffer = device.newBufferWithLength_options_(ctypes.sizeof(renderer.timer), 0)
+    
+    renderer.mouse = (ctypes.c_float * 2)(0.0, 0.0)
+    renderer.mouseBuffer = device.newBufferWithLength_options_(ctypes.sizeof(renderer.mouse), 0)
+    self.mouse = renderer.mouse
+    
 
     return renderer
 
@@ -86,13 +113,18 @@ def drawInMTKView_(_self, _cmd, _view):
   commandEncoder = commandBuffer.computeCommandEncoder()
   commandEncoder.setComputePipelineState_(self.cps)
   commandEncoder.setTexture_atIndex_(drawable.texture(), 0)
+  commandEncoder.setBuffer_offset_atIndex_(self.mouseBuffer, 0, 2)
   commandEncoder.setBuffer_offset_atIndex_(self.timerBuffer, 0, 1)
 
   # --- update
   self.timer.value += 0.01
   bufferPointer = self.timerBuffer.contents()
-  #memcpy(bufferPointer, ctypes.byref(self.timer), ctypes.sizeof(self.timer))
   ctypes.memmove(bufferPointer, ctypes.byref(self.timer), ctypes.sizeof(self.timer))
+  
+  bufferPointer = self.mouseBuffer.contents()
+  ctypes.memmove(bufferPointer, ctypes.byref(self.mouse), ctypes.sizeof(self.mouse))
+  
+  
 
   _width = 8
   _height = 8
