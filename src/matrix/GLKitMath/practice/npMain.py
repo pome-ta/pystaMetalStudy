ee16635a89b2294bda203dc5741291e719e57067
pome-ta <pome-ta@users.noreply.github.com>
2021-08-08 14:30:39 +0900

    Shaders01.py

---
+++ src/matrix/GLKitMath/practice/npMain.py
@@ -0,0 +1,140 @@
+from pathlib import Path
+import ctypes
+
+import numpy as np
+
+from objc_util import c, create_objc_class, ObjCClass, ObjCInstance, ns
+import ui
+
+import pdbg
+
+# --- load Shader code
+shader_path = Path('./Shaders01.py')
+
+# --- load objc classes
+MTKView = ObjCClass('MTKView')
+MTLRenderPipelineDescriptor = ObjCClass('MTLRenderPipelineDescriptor')
+
+# todo: MetalDevice
+MTLCreateSystemDefaultDevice = c.MTLCreateSystemDefaultDevice
+MTLCreateSystemDefaultDevice.argtypes = []
+MTLCreateSystemDefaultDevice.restype = ctypes.c_void_p
+
+err_ptr = ctypes.c_void_p()
+
+# --- Structure
+Vertices = ((ctypes.c_float * 7) * 4)
+vertices_array = [[ 1.0, -1.0, 0.0, 1.0, 0.0, 0.0, 1.0],
+                  [ 1.0,  1.0, 0.0, 0.0, 1.0, 0.0, 1.0],
+                  [-1.0,  1.0, 0.0, 0.0, 0.0, 1.0, 1.0],
+                  [-1.0, -1.0, 0.0, 0.0, 0.0, 0.0, 1.0]]
+np_vertices = np.array(vertices_array, dtype=np.float32)
+verticesData = np_vertices.ctypes.data_as(ctypes.POINTER(Vertices)).contents
+
+Indices = (ctypes.c_uint32 * 6)
+indices_array = [0, 1, 2, 2, 3, 0]
+np_indices = np.array(indices_array, dtype=np.uint32)
+indicesData = np_indices.ctypes.data_as(ctypes.POINTER(Indices)).contents
+
+
+class MetalView(ui.View):
+  def __init__(self, *args, **kwargs):
+    ui.View.__init__(self, *args, **kwargs)
+    self.bg_color = 'maroon'
+    self.view_did_load()
+
+  def view_did_load(self):
+    mtkView = MTKView.alloc()
+    _device = MTLCreateSystemDefaultDevice()
+    devices = ObjCInstance(_device)
+    '''
+    # todo: 端末サイズにて要調整
+    _uw, _uh = ui.get_window_size()
+    _w = min(_uw, _uh) * 0.96
+    _x = (_uw - _w) / 2
+    _y = _uh / 4
+    _frame = ((_x, _y), (_w, _w))
+    '''
+    _frame = ((0.0, 0.0), (128.0, 128.0))
+
+    mtkView.initWithFrame_device_(_frame, devices)
+    mtkView.setAutoresizingMask_((1 << 1) | (1 << 4))
+
+    renderer = self.renderer_init(PyRenderer, mtkView)
+    mtkView.delegate = renderer
+    #mtkView.enableSetNeedsDisplay = True
+    #mtkView.framebufferOnly = False
+    #mtkView.setNeedsDisplay()
+    self.objc_instance.addSubview_(mtkView)
+
+  def renderer_init(self, delegate, _mtkView):
+    renderer = delegate.alloc().init()
+    device = _mtkView.device()
+    renderer.commandQueue = device.newCommandQueue()
+
+    renderer.vertexBuffer = device.newBufferWithBytes_length_options_(
+      verticesData, np_vertices.nbytes, 0)
+    renderer.indicesBuffer = device.newBufferWithBytes_length_options_(
+      indicesData, np_indices.nbytes, 0)
+
+    source = shader_path.read_text('utf-8')
+    library = device.newLibraryWithSource_options_error_(
+      source, err_ptr, err_ptr)
+
+    vertex_func = library.newFunctionWithName_('basic_vertex')
+    frag_func = library.newFunctionWithName_('basic_fragment')
+
+    rpld = MTLRenderPipelineDescriptor.new()
+    rpld.vertexFunction = vertex_func
+    rpld.fragmentFunction = frag_func
+    rpld.colorAttachments().objectAtIndexedSubscript(
+      0).pixelFormat = 80  # .bgra8Unorm
+
+    renderer.rps = device.newRenderPipelineStateWithDescriptor_error_(
+      rpld, err_ptr)
+
+    return renderer
+
+
+# --- MTKViewDelegate
+def drawInMTKView_(_self, _cmd, _view):
+  self = ObjCInstance(_self)
+  view = ObjCInstance(_view)
+
+  drawable = view.currentDrawable()
+  rpd = view.currentRenderPassDescriptor()
+  rpd.colorAttachments().objectAtIndexedSubscript(
+    0).texture = drawable.texture()
+  rpd.colorAttachments().objectAtIndexedSubscript(
+    0).loadAction = 2  # .clear
+  rpd.colorAttachments().objectAtIndexedSubscript(
+    0).clearColor = (0.85, 0.85, 0.85, 1.0)
+
+  commandBuffer = self.commandQueue.commandBuffer()
+  commandEncoder = commandBuffer.renderCommandEncoderWithDescriptor_(rpd)
+
+  commandEncoder.setRenderPipelineState_(self.rps)
+
+  commandEncoder.setVertexBuffer_offset_atIndex_(self.vertexBuffer, 0, 0)
+  commandEncoder.drawIndexedPrimitives_indexCount_indexType_indexBuffer_indexBufferOffset_(
+    3, 6, 1, self.indicesBuffer, 0)
+
+  commandEncoder.endEncoding()
+  commandBuffer.presentDrawable_(drawable)
+  commandBuffer.commit()
+
+
+def mtkView_drawableSizeWillChange_(_self, _cmd, _view, _size):
+  self = ObjCInstance(_self)
+  view = ObjCInstance(_view)
+
+
+PyRenderer = create_objc_class(
+  name='PyRenderer',
+  methods=[drawInMTKView_, mtkView_drawableSizeWillChange_],
+  protocols=['MTKViewDelegate'])
+
+if __name__ == '__main__':
+  view = MetalView()
+  view.present(style='fullscreen', orientations=['portrait'])
+
