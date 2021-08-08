108a99a340e01574c2791e23926d5c01f745497a
pome-ta <pome-ta@users.noreply.github.com>
2021-08-08 22:53:33 +0900

    matrixMain.py

--- src/matrix/GLKitMath/practice/matrixMain.py
+++ src/matrix/GLKitMath/practice/matrixMain.py
@@ -8,7 +8,7 @@ import ui
 import pdbg
 
 # --- load Shader code
-shader_path = Path('./Shaders01.py')
+shader_path = Path('./Shaders02.py')
 
 # --- load objc classes
 MTKView = ObjCClass('MTKView')
@@ -23,6 +23,7 @@ err_ptr = ctypes.c_void_p()
 
 # --- GLKit MathMatrix
 # https://github.com/Cethric/OpenGLES-Pythonista/blob/master/GLKit/glkmath/matrix4.py
+
 class float16(ctypes.Structure):
   _fields_ = [('m', (ctypes.c_float * 16)),]
 
@@ -75,7 +76,6 @@ def GLKMatrix4Make(m00, m01, m02, m03, m10, m11, m12, m13, m20, m21, m22, m23, m
 def GLKMatrix4MakeWithArray(values):
   return GLKMatrix4Make(*values)
 
-
 def GLKMatrix4Identity():
   return GLKMatrix4Make(1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1)
 
@@ -86,6 +86,73 @@ def GLKMatrix4MakeTranslation(tx, ty, tz):
   m.m[14] = tz
   return m
 
+class xyz(ctypes.Structure):
+  _fields_ = [('x', ctypes.c_float),
+              ('y', ctypes.c_float),
+              ('z', ctypes.c_float),]
+                
+class rgb(ctypes.Structure):
+  _fields_ = [('r', ctypes.c_float),
+              ('g', ctypes.c_float),
+              ('b', ctypes.c_float),]
+                
+class stp(ctypes.Structure):
+  _fields_ = [('s', ctypes.c_float),
+              ('t', ctypes.c_float),
+              ('p', ctypes.c_float),]
+                
+class float3(ctypes.Structure):
+  _fields_ = [('v', (ctypes.c_float * 3))]
+
+class GLKVector3(ctypes.Union):
+  _anonymous_ = [('s1'), ('s2'), ('s3'), ('s4'),]
+  _fields_ = [('s1', xyz), ('s2', rgb), ('s3', stp), ('s4', float3),]
+                
+  def __str__(self):
+    r = []
+    s1 = self.s1
+    s2 = self.s2
+    s3 = self.s3
+    s4 = self.s4
+    r.extend([float(x) for x in s4.v])
+    vstr = '''GLKVector3 { %.4f\t%.4f\t%.4f }''' % tuple(r)
+    return vstr
+
+def GLKVector3Make(x, y, z):
+  return GLKVector3(x=x, y=y, z=z)
+
+def GLKVector3Length(vector):
+  v = [float(x) for x in vector.v]
+  r = 0
+  for x in v:
+    r += math.pow(x, 2)
+  return math.sqrt(r)
+
+
+def GLKVector3Normalize(vector):
+  l = GLKVector3Length(vector)
+  nv = GLKVector3()
+  if l != 0:
+    nv.x = vector.x / l
+    nv.y = vector.y / l
+    nv.z = vector.z / l
+    return nv
+  else:
+    raise ValueError('Cannot Normalise Vector of length 0')
+
+def GLKMatrix4MakeRotation(radians, x, y, z):
+  v = GLKVector3Normalize(GLKVector3Make(x, y, z));
+  cos = math.cos(radians);
+  cosp = 1.0 - cos;
+  sin = math.sin(radians);
+  
+  array = [cos + cosp * v.v[0] * v.v[0], cosp * v.v[0] * v.v[1] + v.v[2] * sin, cosp * v.v[0] * v.v[2] - v.v[1] * sin, 0.0,
+           cosp * v.v[0] * v.v[1] - v.v[2] * sin,    cos + cosp * v.v[1] * v.v[1],    cosp * v.v[1] * v.v[2] + v.v[0] * sin,    0.0,
+           cosp * v.v[0] * v.v[2] + v.v[1] * sin, cosp * v.v[1] * v.v[2] - v.v[0] * sin, cos + cosp * v.v[2] * v.v[2], 0.0,
+           0.0, 0.0, 0.0, 1.0]
+
+  return GLKMatrix4MakeWithArray(array)
+
 def GLKMatrix4MakePerspective(fovyRadians, aspect, nearZ, farZ):
   cotan = 1.0 / math.tan(fovyRadians / 2.0);
   array = [cotan / aspect, 0.0, 0.0, 0.0,
@@ -93,19 +160,35 @@ def GLKMatrix4MakePerspective(fovyRadians, aspect, nearZ, farZ):
            0.0, 0.0, (farZ + nearZ) / (nearZ - farZ), -1.0,
            0.0, 0.0, (2.0 * farZ * nearZ) / (nearZ - farZ), 0.0]
   return GLKMatrix4MakeWithArray(array)
+
+def GLKMatrix4Multiply(matrixLeft, matrixRight):
+  m = GLKMatrix4()
+  m.m[0] = matrixLeft.m[0] * matrixRight.m[0] + matrixLeft.m[4] * matrixRight.m[1] + matrixLeft.m[8] * matrixRight.m[2] + matrixLeft.m[12] * matrixRight.m[3]
+  m.m[4]  = matrixLeft.m[0] * matrixRight.m[4]  + matrixLeft.m[4] * matrixRight.m[5]  + matrixLeft.m[8] * matrixRight.m[6]   + matrixLeft.m[12] * matrixRight.m[7];
+  m.m[8]  = matrixLeft.m[0] * matrixRight.m[8]  + matrixLeft.m[4] * matrixRight.m[9]  + matrixLeft.m[8] * matrixRight.m[10]  + matrixLeft.m[12] * matrixRight.m[11];
+  m.m[12] = matrixLeft.m[0] * matrixRight.m[12] + matrixLeft.m[4] * matrixRight.m[13] + matrixLeft.m[8] * matrixRight.m[14]  + matrixLeft.m[12] * matrixRight.m[15];
+  
+  m.m[1]  = matrixLeft.m[1] * matrixRight.m[0]  + matrixLeft.m[5] * matrixRight.m[1]  + matrixLeft.m[9] * matrixRight.m[2]   + matrixLeft.m[13] * matrixRight.m[3];
+  m.m[5]  = matrixLeft.m[1] * matrixRight.m[4]  + matrixLeft.m[5] * matrixRight.m[5]  + matrixLeft.m[9] * matrixRight.m[6]   + matrixLeft.m[13] * matrixRight.m[7];
+  m.m[9]  = matrixLeft.m[1] * matrixRight.m[8]  + matrixLeft.m[5] * matrixRight.m[9]  + matrixLeft.m[9] * matrixRight.m[10]  + matrixLeft.m[13] * matrixRight.m[11];
+  m.m[13] = matrixLeft.m[1] * matrixRight.m[12] + matrixLeft.m[5] * matrixRight.m[13] + matrixLeft.m[9] * matrixRight.m[14]  + matrixLeft.m[13] * matrixRight.m[15];
+  
+  m.m[2]  = matrixLeft.m[2] * matrixRight.m[0]  + matrixLeft.m[6] * matrixRight.m[1]  + matrixLeft.m[10] * matrixRight.m[2]  + matrixLeft.m[14] * matrixRight.m[3];
+  m.m[6]  = matrixLeft.m[2] * matrixRight.m[4]  + matrixLeft.m[6] * matrixRight.m[5]  + matrixLeft.m[10] * matrixRight.m[6]  + matrixLeft.m[14] * matrixRight.m[7];
+  m.m[10] = matrixLeft.m[2] * matrixRight.m[8]  + matrixLeft.m[6] * matrixRight.m[9]  + matrixLeft.m[10] * matrixRight.m[10] + matrixLeft.m[14] * matrixRight.m[11];
+  m.m[14] = matrixLeft.m[2] * matrixRight.m[12] + matrixLeft.m[6] * matrixRight.m[13] + matrixLeft.m[10] * matrixRight.m[14] + matrixLeft.m[14] * matrixRight.m[15];
+  
+  m.m[3]  = matrixLeft.m[3] * matrixRight.m[0]  + matrixLeft.m[7] * matrixRight.m[1]  + matrixLeft.m[11] * matrixRight.m[2]  + matrixLeft.m[15] * matrixRight.m[3];
+  m.m[7]  = matrixLeft.m[3] * matrixRight.m[4]  + matrixLeft.m[7] * matrixRight.m[5]  + matrixLeft.m[11] * matrixRight.m[6]  + matrixLeft.m[15] * matrixRight.m[7];
+  m.m[11] = matrixLeft.m[3] * matrixRight.m[8]  + matrixLeft.m[7] * matrixRight.m[9]  + matrixLeft.m[11] * matrixRight.m[10] + matrixLeft.m[15] * matrixRight.m[11];
+  m.m[15] = matrixLeft.m[3] * matrixRight.m[12] + matrixLeft.m[7] * matrixRight.m[13] + matrixLeft.m[11] * matrixRight.m[14] + matrixLeft.m[15] * matrixRight.m[15];
+  
+  return m;
+
+def GLKMatrix4Rotate(matrix, radians, x, y, z):
+  rm = GLKMatrix4MakeRotation(radians, x, y, z);
+  return GLKMatrix4Multiply(matrix, rm);
     
-
-
-
-# --- Structure
-vertices = ((ctypes.c_float * 7) * 4)(
-  ( 1.0, -1.0, 0.0, 1.0, 0.0, 0.0, 1.0),
-  ( 1.0,  1.0, 0.0, 0.0, 1.0, 0.0, 1.0),
-  (-1.0,  1.0, 0.0, 0.0, 0.0, 1.0, 1.0),
-  (-1.0, -1.0, 0.0, 0.0, 0.0, 0.0, 1.0))
-
-
-indices = (ctypes.c_uint32 * 6)(0, 1, 2, 2, 3, 0)
 
 class SceneMatrices(ctypes.Structure):
   # todo: add `def __str__(self):` ?
@@ -118,7 +201,17 @@ class SceneMatrices(ctypes.Structure):
     self.projectionMatrix = GLKMatrix4Identity()
     self.modelviewMatrix = GLKMatrix4Identity()
 
+
+# --- Structure
+vertices = ((ctypes.c_float * 7) * 4)(
+  ( 1.0, -1.0, 0.0, 1.0, 0.0, 0.0, 1.0),
+  ( 1.0,  1.0, 0.0, 0.0, 1.0, 0.0, 1.0),
+  (-1.0,  1.0, 0.0, 0.0, 0.0, 1.0, 1.0),
+  (-1.0, -1.0, 0.0, 0.0, 0.0, 0.0, 1.0))
+indices = (ctypes.c_uint32 * 6)(0, 1, 2, 2, 3, 0)
+
 sceneMatrices = SceneMatrices()
+
 
 
 class MetalView(ui.View):
@@ -180,7 +273,6 @@ class MetalView(ui.View):
     renderer.rotation = 0.0
     renderer.preferredFramesTime = 1.0 / _mtkView.preferredFramesPerSecond()
 
-
     return renderer
 
 
@@ -204,6 +296,14 @@ def drawInMTKView_(_self, _cmd, _view):
   commandEncoder.setRenderPipelineState_(self.rps)
 
   commandEncoder.setVertexBuffer_offset_atIndex_(self.vertexBuffer, 0, 0)
+  
+  modelViewMatrix = GLKMatrix4MakeTranslation(0.0, 0.0, -6.0)
+  sceneMatrices.modelviewMatrix = modelViewMatrix
+  
+  uniformBuffer = view.device().newBufferWithBytes_length_options_(ctypes.byref(sceneMatrices), ctypes.sizeof(sceneMatrices), 0)
+  
+  commandEncoder.setVertexBuffer_offset_atIndex_(uniformBuffer, 0, 1)
+  
   commandEncoder.drawIndexedPrimitives_indexCount_indexType_indexBuffer_indexBufferOffset_(
     3, 6, 1, self.indicesBuffer, 0)
 
@@ -215,6 +315,12 @@ def drawInMTKView_(_self, _cmd, _view):
 def mtkView_drawableSizeWillChange_(_self, _cmd, _view, _size):
   self = ObjCInstance(_self)
   view = ObjCInstance(_view)
+  _width = _size.width
+  _height = _size.height
+  aspect = math.fabs(_width / _height)
+  projectionMatrix = GLKMatrix4MakePerspective(math.radians(65.0), aspect, 4.0, 10.0)
+  sceneMatrices.projectionMatrix = projectionMatrix
+  
 
 
 PyRenderer = create_objc_class(
