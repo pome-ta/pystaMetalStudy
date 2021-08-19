1d1f058c654da0b1cb28b9a651e178f5e4c7a146
pome-ta <pome-ta@users.noreply.github.com>
2021-08-19 12:01:31 +0900

    up

--- src/beginningMetal/04_IndicesAndConstants/final.py
+++ src/beginningMetal/04_IndicesAndConstants/final.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from math import sin
 import ctypes
 
 from objc_util import c, create_objc_class, ObjCClass, ObjCInstance
@@ -7,7 +8,8 @@ import ui
 import pdbg
 
 # --- get Shader path
-shader_path = Path('./challenge_Shader.metal')
+shader_path = Path('./final_Shader.metal')
+
 
 # --- load objc classes
 MTKView = ObjCClass('MTKView')
@@ -20,43 +22,30 @@ MTLCreateSystemDefaultDevice.restype = ctypes.c_void_p
 
 err_ptr = ctypes.c_void_p()
 
-
 wenderlichGreen = (0.0, 0.4, 0.21, 1.0)
-'''
-vertices = (ctypes.c_float * (3 * 6))(
+
+vertices = (ctypes.c_float * 12)(
   -1.0,  1.0, 0.0,    # v0
   -1.0, -1.0, 0.0,    # v1
    1.0, -1.0, 0.0,    # v2
-   1.0, -1.0, 0.0,    # v2
-   1.0,  1.0, 0.0,    # v3
-  -1.0,  1.0, 0.0,    # v0
-   )
+   1.0,  1.0, 0.0,)   # v3
 '''
-vertices = (ctypes.c_float * (3 * 6))(
+vertices = (ctypes.c_float * 12)(
   -0.8,  0.8, 0.0,    # v0
   -0.8, -0.8, 0.0,    # v1
    0.8, -0.8, 0.0,    # v2
-   0.8, -0.8, 0.0,    # v2
-   0.8,  0.8, 0.0,    # v3
-  -0.8,  0.8, 0.0,    # v0
-   )
+   0.8,  0.8, 0.0,)   # v3
+'''
+
+indices = (ctypes.c_int16 * 6)(0, 1, 2, 2, 3, 0)
 
 
-'''
-   v0                v3
-(-1, 1)--( 0, 1)--( 1, 1)
-   |  \               |
-   |    \             |
-   |      \           |
-   |        \         |
-(-1, 0)  ( 0, 0)  ( 1, 0)
-   |          \       |
-   |            \     |
-   |              \   |
-   |                \ |
-(-1,-1)--( 0,-1)--( 1,-1)
-   v1                v2
-'''
+class Constants(ctypes.Structure):
+  _fields_ = [('animateBy', ctypes.c_float)]
+
+
+constants = Constants()
+animateBy = (ctypes.c_float)(0.0)
 
 
 class PyMetal(ui.View):
@@ -82,11 +71,15 @@ class PyMetal(ui.View):
     renderer = PyRenderer.alloc().init()
     renderer.commandQueue = device.newCommandQueue()
 
+    renderer.time = 0.0
+
     # --- buildModel
-    
-    renderer.vertexBuffer = device.newBufferWithBytes_length_options_(vertices, vertices.__len__() * ctypes.sizeof(vertices), 0)
-    
-    
+    renderer.vertexBuffer = device.newBufferWithBytes_length_options_(
+      vertices, vertices.__len__() * ctypes.sizeof(vertices), 0)
+
+    renderer.indexBuffer = device.newBufferWithBytes_length_options_(
+      indices, indices.__len__() * ctypes.sizeof(indices), 0)
+
     # --- buildPipelineState
     source = shader_path.read_text('utf-8')
     library = device.newLibraryWithSource_options_error_(
@@ -116,12 +109,19 @@ def drawInMTKView_(_self, _cmd, _view):
   view = ObjCInstance(_view)
   drawable = view.currentDrawable()
   rpd = view.currentRenderPassDescriptor()
+
+  self.time += 1 / view.preferredFramesPerSecond()
+  animateBy.value = abs(sin(self.time) / 2 + 0.5)
+  constants.animateBy = animateBy
+
   commandBuffer = self.commandQueue.commandBuffer()
   commandEncoder = commandBuffer.renderCommandEncoderWithDescriptor_(rpd)
   commandEncoder.setRenderPipelineState_(self.rps)
   commandEncoder.setVertexBuffer_offset_atIndex_(self.vertexBuffer, 0, 0)
-  commandEncoder.drawPrimitives_vertexStart_vertexCount_(
-    2, 0, vertices.__len__())  # .triangle
+  commandEncoder.setVertexBytes_length_atIndex_(
+    ctypes.byref(constants), ctypes.sizeof(constants), 1)
+  commandEncoder.drawIndexedPrimitives_indexCount_indexType_indexBuffer_indexBufferOffset_(
+    3, indices.__len__(), 0, self.indexBuffer, 0)
 
   commandEncoder.endEncoding()
   commandBuffer.presentDrawable_(drawable)
