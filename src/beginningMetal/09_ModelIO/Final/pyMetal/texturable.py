9d577ce8aa21f89308cd089a6efcfc0fc099d2ae
pome-ta <pome-ta@users.noreply.github.com>
2021-09-22 23:27:21 +0900

    glob search

--- src/beginningMetal/09_ModelIO/Final/pyMetal/texturable.py
+++ src/beginningMetal/09_ModelIO/Final/pyMetal/texturable.py
@@ -4,15 +4,17 @@ from objc_util import ObjCClass, ns, nsurl
 
 from .utils import err_ptr
 
+path_list = Path.cwd().glob('./**')
+
 
 class Texturable:
   def setTexture_device_imageName_(self, device, imageName):
     # xxx: Shader path もやる？
     def get_image_path(_imageName):
-      root = Path('./Images')
-      for file in root.iterdir():
-        if file.name == _imageName:
-          return file.absolute()
+      for file_path in path_list:
+        for file in file_path.iterdir():
+          if file.name == _imageName:
+            return file.absolute()
 
     textureLoader = ObjCClass('MTKTextureLoader').new()
     textureLoader.initWithDevice_(device)
