29dbd3e1b2aa1bc75314738daa7b6da726acd226
pome-ta <pome-ta@users.noreply.github.com>
2021-11-23 23:13:22 +0900

    human random

--- src/beginningMetal/10_Instances/Final/pyMetal/instanceScene.py
+++ src/beginningMetal/10_Instances/Final/pyMetal/instanceScene.py
@@ -1,3 +1,5 @@
+from random import random, randrange
+
 from .structures import Float3
 from .instance import Instance
 from .mScene import Scene
@@ -5,15 +7,18 @@ from .mScene import Scene
 
 class InstanceScene(Scene):
   def __init__(self, device, size):
-    self.humans = Instance(device, 'humanFigure', 40)
+    self.humans = Instance(device, 'humanFigure', 1)
     super().__init__(device, size)
     self.add_childNode_(self.humans)
     
     for human in self.humans.nodes:
+      #s_num = randrange(5) / 10
+      #human.scale = Float3(s_num, s_num, s_num)
+      # xxx: 確認用
       human.scale = Float3(0.5, 0.5, 0.5)
-      human.position.x = -2.0
-      human.position.y = -3.0
-      human.materialColor = (0.5, 0.0, 1.0, 1.0)
+      human.position.x = float(randrange(5)) - 2.0
+      human.position.y = float(randrange(5)) - 3.0
+      human.materialColor = (random(), random(), random(), 1.0)
   
   def update_deltaTime_(self, deltaTime):
     # todo: 親の`Scene` が`pass` だけどとりあえず
