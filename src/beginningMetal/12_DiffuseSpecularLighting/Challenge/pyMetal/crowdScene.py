3ec8175394d768b3dad418b62192595cb1ba2c94
pome-ta <pome-ta@users.noreply.github.com>
2021-12-02 00:13:15 +0900

    challenge

---
+++ src/beginningMetal/12_DiffuseSpecularLighting/Challenge/pyMetal/crowdScene.py
@@ -0,0 +1,26 @@
+from random import random, randrange
+
+from .mScene import Scene
+from .model import Model
+from .structures import Float3
+
+
+class CrowdScene(Scene):
+  def __init__(self, device, size):
+    self.humans = []
+    # self.humans = Model(device, 'humanFigure')
+    super().__init__(device, size)
+    for _ in range(40):
+      human = Model(device, 'humanFigure')
+      self.humans.append(human)
+      self.add_childNode_(human)
+      s_num = randrange(5) / 10
+      human.scale = Float3(s_num, s_num, s_num)
+      human.position.x = float(randrange(5)) - 2.0
+      human.position.y = float(randrange(5)) - 3.0
+      r, g, b = [random(), random(), random()]
+      human.materialColor = (r, g, b, 1.0)
+  
+  def update_deltaTime_(self, deltaTime):
+    # todo: 親の`Scene` が`pass` だけどとりあえず
+    super().update_deltaTime_(deltaTime)
---
+++ src/beginningMetal/12_DiffuseSpecularLighting/Challenge/pyMetal/pyTypes.py
@@ -0,0 +1,35 @@
+import ctypes
+
+from .matrixMath import MatrixFloat4x4 as MatrixIdentityFloat4x4
+from .structures import Position, Color, Texture, Float4, Float3, MatrixFloat3x3
+
+
+class Vertex(ctypes.Structure):
+  _fields_ = [('position', Position), ('color', Color), ('texture', Texture)]
+
+
+# matrix_identity_float4x4 <= 雰囲気
+class ModelConstants(ctypes.Structure):
+  _fields_ = [
+    ('modelViewMatrix', MatrixIdentityFloat4x4),
+    ('materialColor', Float4),
+    ('normalMatrix', MatrixFloat3x3),
+    ('specularIntensity', ctypes.c_float),
+    ('shininess', ctypes.c_float)
+  ]
+
+
+class SceneConstants(ctypes.Structure):
+  _fields_ = [
+    ('projectionMatrix', MatrixIdentityFloat4x4),
+  ]
+
+
+class Light(ctypes.Structure):
+  _fields_ = [
+    ('color', Float3),
+    ('ambientIntensity', ctypes.c_float),
+    ('diffuseIntensity', ctypes.c_float),
+    ('direction', Float3)
+  ]
+
