4ffbce26f235dd8a10ca91d085dbb9aeb89eb88f
pome-ta <pome-ta@users.noreply.github.com>
2021-12-02 16:49:26 +0900

    __main__.py

--- src/v2/01_Hello_Metal/__main__.py
+++ src/v2/01_Hello_Metal/__main__.py
@@ -1,9 +1,14 @@
 import ctypes
 
-from objc_util import c, ObjCClass, ObjCInstance
+from objc_util import c, ObjCClass, ObjCInstance, load_framework
 import ui
 
 import pdbg
+
+
+load_framework('')
+
+err_ptr = ctypes.c_void_p()
 
 MTLCreateSystemDefaultDevice = c.MTLCreateSystemDefaultDevice
 MTLCreateSystemDefaultDevice.argtypes = []
@@ -21,8 +26,39 @@ MTKMeshBufferAllocator = ObjCClass('MTKMeshBufferAllocator')
 allocator = MTKMeshBufferAllocator.new().initWithDevice_(device)
 
 MDLMesh = ObjCClass('MDLMesh')
-mdlMesh = MDLMesh.new()
+#mdlMesh = MDLMesh.new()
 
+commandQueue = device.newCommandQueue()
+
+shader = '''
+#include <metal_stdlib>
+using namespace metal;
+struct VertexIn {
+  float4 position [[ attribute(0) ]];
+};
+vertex float4 vertex_main(const VertexIn vertex_in [[ stage_in ]]) {
+  return vertex_in.position;
+}
+fragment float4 fragment_main() {
+  return float4(1, 0, 0, 1);
+}
+'''
+
+
+library = device.newLibraryWithSource_options_error_(shader, err_ptr, err_ptr)
+
+vertexFunction = library.newFunctionWithName_('vertex_main')
+fragmentFunction = library.newFunctionWithName_('fragment_main')
+
+MTLRenderPipelineDescriptor = ObjCClass('MTLRenderPipelineDescriptor')
+
+pipelineDescriptor = MTLRenderPipelineDescriptor.new()
+
+# MTLPixelFormatBGRA8Unorm
+pipelineDescriptor.colorAttachments().objectAtIndexedSubscript_(0).pixelFormat = 80
+
+pipelineDescriptor.vertexFunction = vertexFunction
+pipelineDescriptor.fragmentFunction = fragmentFunction
 
 
 
