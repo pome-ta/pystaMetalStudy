6fc7a97d0958ea2805739d1b7f60e85346477aaa
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-11-02 13:54:49 +0900

    day02

---
+++ src/thirtyDaysOfMetal/02/memo.md
@@ -0,0 +1,5 @@
+# 📝 2023/11/02
+
+[Thirty Days of Metal — Day 2: Buffers | by Warren Moore | Medium](https://medium.com/@warrenm/thirty-days-of-metal-day-2-buffers-ec8c81040e3e)
+
+
---
+++ src/thirtyDaysOfMetal/02/sandbox/231102_1337.py
@@ -0,0 +1,22 @@
+import ctypes
+from objc_util import c, ObjCInstance
+
+import pdbg
+
+
+def MTLCreateSystemDefaultDevice():
+  _MTLCreateSystemDefaultDevice = c.MTLCreateSystemDefaultDevice
+  _MTLCreateSystemDefaultDevice.argtypes = []
+  _MTLCreateSystemDefaultDevice.restype = ctypes.c_void_p
+  return ObjCInstance(_MTLCreateSystemDefaultDevice())
+
+
+device = MTLCreateSystemDefaultDevice()
+
+buffer = device.newBufferWithLength(16, options=0)
+
+#print(f'Buffer is {buffer.length()} bytes in length')
+#pdbg.state(buffer)
+
+contents = ObjCInstance(buffer.contents())
+pdbg.state(contents)
