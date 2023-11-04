d3a8d8b60dce913b82400b3b68dde9b45573e6fb
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-11-04 11:08:36 +0900

    up

--- src/thirtyDaysOfMetal/06/sandbox/231103_1951.py
+++ src/thirtyDaysOfMetal/06/sandbox/231103_1951.py
@@ -1,1 +1,29 @@
+from pathlib import Path
+import ctypes
+
+from objc_util import c, ObjCInstance
+
+import pdbg
+
+shader_path = Path('./Shaders.metal')
+
+err_ptr = ctypes.c_void_p()
+
+
+def MTLCreateSystemDefaultDevice():
+  _MTLCreateSystemDefaultDevice = c.MTLCreateSystemDefaultDevice
+  _MTLCreateSystemDefaultDevice.argtypes = []
+  _MTLCreateSystemDefaultDevice.restype = ctypes.c_void_p
+  return ObjCInstance(_MTLCreateSystemDefaultDevice())
+
+
+device = MTLCreateSystemDefaultDevice()
+source = shader_path.read_text('utf-8')
+
+library = device.newLibraryWithSource(source, options=None, error=err_ptr)
+
+for name in library.functionNames():
+  function = library.newFunctionWithName(name)
+  print(function)
+
 
